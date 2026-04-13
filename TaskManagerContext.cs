127498fb3ffbd47fdaa4d87d9c9aad105336c88c
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-13 14:40:54 -0400

    simple config

---
+++ TaskManagerContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+//using TaskManager.Models;
+
+namespace TaskManager
+{
+    public class TaskManagerContext : DbContext
+    {
+        public DbSet<Models.Priority> Priorities { get; set;  }
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection_string = "";
+
+            string database_name = "TaskManagerDB";
+
+            optionsBuilder.UseSqlServer($"{connection_string};Database={database_name}");
+        }
+
+    }
+}
