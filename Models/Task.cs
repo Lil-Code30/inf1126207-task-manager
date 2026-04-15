db615d0b4ae780f21213e9577d9162ec9209dbaa
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-14 21:40:00 -0400

    feat: Add validation attributes to Task and User models
    Annotate model properties with data annotation attributes to enforce validation: add using System.ComponentModel.DataAnnotations; mark Task.TaskId, Task.Title, and Task.DueDate with [Required]; mark User.UserId and User.Name with [Required] and constrain User.Name with [StringLength(100)]. This enables model-level validation for tasks and users.

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TaskManager.Models
 {
     public class Task
     {
+        [Required]
         public int TaskId { get; set; }
+        
+        [Required]
         public string Title { get; set; }
         public bool Status { get; set; }
+
+        [Required]
         public DateTime DueDate { get; set; }
         
         // Task <-> User Relation many-to-many
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TaskManager.Models
 {
     public class User
     {
+        [Required]
         public int UserId { get; set; }
+        
+        [Required]
+        [StringLength(100)]
         public string Name { get; set;  }
         
         // User <-> Task Relation : one-to-many
